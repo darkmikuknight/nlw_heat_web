74758c58ea7308058a23213244c195a50a3ab851
Guilherme Ribeiro <guilherme.ribeiro@ciadoleite.com>
2021-10-21 20:08:11 -0300

    mais ajuste de estiliazação e adicão do login usando github

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -3,10 +3,12 @@ import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss'
 
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=213b95456777dc0bb425&redirect_uri=http://localhost:3000`;
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com GitHub
             </a>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,42 +1,46 @@
-import styles from './styles.module.scss'
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 
+import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        nome: string;
+        avatar_url: string;
+    }
+}
+
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            console.log(response.data);
+            setMessages(response.data);
+        })
+    }, [])
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.messageImage}>
-                            <img src="https://github.com/diego3g.png" alt="user image" />
-                        </div>
-                        <span>Diego Fernandes</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.messageImage}>
-                            <img src="https://github.com/diego3g.png" alt="user image" />
-                        </div>
-                        <span>Diego Fernandes</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.messageImage}>
-                            <img src="https://github.com/diego3g.png" alt="user image" />
-                        </div>
-                        <span>Diego Fernandes</span>
-                    </div>
-                </li>
+                {messages.map(messages => {
+                    return (  
+                        <li key={messages.id} className={styles.message}>
+                            <p className={styles.messageContent}>{messages.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={messages.user.avatar_url} alt={messages.user.nome} />
+                                </div>
+                                <span>{messages.user.nome}</span>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     )
