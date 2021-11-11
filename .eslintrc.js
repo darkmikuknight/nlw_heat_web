a1a2abad9823145261025def7bd46643ded21ad8
Guilherme <guilhemerps94@gmail.com>
2021-11-10 23:35:10 -0300

    Mais ajustes de eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,38 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser', // Especifica o analisador ESLint
     parserOptions: {
-        ecmaVersion: 2020, // Permite a análise de recursos modernos do ECMAScript
-        sourceType: 'module', // Permite o uso de imports
+        ecmaVersion: 2020,
+        sourceType: 'module' // Permite o uso de imports
     },
     env: {
         es6: true,
-        node: true,
+        node: true
     },
     plugins: [
-        '@typescript-eslint',
+        '@typescript-eslint'
         // 'prettier'
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        // 'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
     globals: {
         Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
     rules: {
+        // 'prettier/prettier': ['error'],
+        'no-constant-condition': 'error',
         'quotes': ['error', 'single'],
         'indent': ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        'eqeqeq': ['error', 'always'],
+        'linebreak-style': ['error', 'unix'],
+        'no-unexpected-multiline': 'error',
+        'no-mixed-operators': 'error',
         'no-unused-vars': 'warn',
         'no-unused-expressions': 'off',
         'func-names': 'off',
@@ -33,42 +42,88 @@ module.exports = {
         'no-multi-spaces': 'error',
         'no-trailing-spaces': 'error',
         'space-before-blocks': 'error',
-        'no-multiple-empty-lines': ['error', {
-            'max': 1,
-            'maxEOF': 0
+        'dot-notation': 'error',
+        'no-undef': 'error',
+        'prefer-const': 'error',
+        'no-plusplus': 'error',
+        'no-multiple-empty-lines': [
+            'error',
+            {
+                max: 1,
+                maxEOF: 0
+            }
+        ],
+        'comma-spacing': [
+            'error',
+            {
+                before: false,
+                after: true
+            }
+        ],
+        'max-len': [2, {
+            code: 100
         }],
-        'comma-spacing': ['error', {
-            'before': false,
-            'after': true
-        }],
+        // 'max-len': ['error', { 'code': 80, 'ignoreUrls': true }],
         // 'array-bracket-spacing': ['error', 'never'],
         // 'semi': ['error', 'always'],
-        // 'max-len': ['error', {
-        //     'code': 90,
-        // }],
+        // 'operator-linebreak': ['error', 'after']
         'object-curly-spacing': ['error', 'always'],
-        'space-before-function-paren': ['error', {
-            'anonymous': 'always',
-            'named': 'always',
-            'asyncArrow': 'always'
-        }],
-        'key-spacing': ['error', {
-            'beforeColon': false,
-            'afterColon': true,
-            'mode': 'strict',
-        }],
-        'spaced-comment': ['error', 'always', {
-            'line': {
-                'markers': ['/'],
-                'exceptions': ['-', '+']
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'always',
+                named: 'always',
+                asyncArrow: 'always'
+            }
+        ],
+        'key-spacing': [
+            'error',
+            {
+                beforeColon: false,
+                afterColon: true,
+                mode: 'strict'
+            }
+        ],
+        'spaced-comment': [
+            'error',
+            'always',
+            {
+                line: {
+                    markers: ['/'],
+                    exceptions: ['-', '+']
+                },
+                block: {
+                    markers: ['!'],
+                    exceptions: ['*'],
+                    balanced: true
+                }
+            }
+        ],
+        'object-curly-newline': ['error', {
+            'ObjectExpression': 'always',
+            'ObjectPattern': {
+                'multiline': true, 'minProperties': 3
             },
-            'block': {
-                'markers': ['!'],
-                'exceptions': ['*'],
-                'balanced': true
+            'ImportDeclaration': 'never',
+            'ExportDeclaration': {
+                'multiline': true, 'minProperties': 3
             }
         }],
-    },
+        'nonblock-statement-body-position': ['error', 'below'],
+        'no-else-return': 'error',
+        'eol-last': ['error', 'always'],
+        'no-extra-parens': ['error', 'all', {
+            'conditionalAssign': false,
+            'returnAssign': false,
+            'ignoreJSX': 'multi-line'
+        }]
+        "react-hooks/rules-of-hooks": "error",
+        "import/prefer-default-export": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".ts", ".jsx", ".tsx"] }],
+        "react/jsx-indent": ["off", 4],
+        "react/jsx-indent-props": ["off"],
+
+    }
     // settings: {
     //     'import/resolver': {
     //         'node': {
--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,19 +1,18 @@
-import { VscGithubInverted } from 'react-icons/vsc'
+import { VscGithubInverted } from 'react-icons/vsc';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 export function LoginBox () {
     const signInUrl = 'https://github.com/login/oauth/authorize?scope=user&client_id=213b95456777dc0bb425&redirect_uri=http://localhost:3000';
 
+    if (false) {
+        console.log('tetete');
+    }
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href={signInUrl} className={styles.signInWithGithub}>
-                <VscGithubInverted size="24" />
-                Entrar com GitHub
-            </a>
-
+            <a href={signInUrl} className={styles.signInWithGithub}><VscGithubInverted size="24" />Entrar com GitHub</a>
         </div>
-
-    )
+    );
 }
